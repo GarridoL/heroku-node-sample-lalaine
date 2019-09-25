686a1c659d628fa1dd4a1b7a8959f154c6885657
mmayutay <raymondjay.yorong@student.passerellesnumeriques.org>
2019-09-25 14:35:37 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){ 
     console.log(msg)
-    io.emit('chat message', msg)   
+    io.emit('chat message', msg)
     
   });
   socket.on('add user', function(data){
