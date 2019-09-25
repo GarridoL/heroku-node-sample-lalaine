b226c8cd3a0bc6505e46430b211e7d01f960f49f
mmayutay <raymondjay.yorong@student.passerellesnumeriques.org>
2019-09-25 13:57:39 +0800

    Sample

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').createServer(app);
 var port = process.env.port || 3000;
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(port, '0.0.0.0,', function(){
